380743a75b85ec7d3124a4d99566d616687dbea4
jmeyer <jmeyer@marketo.com>
2019-10-08 08:03:06 -0600

    ip in network test

---
+++ python/ip_in_network.py
@@ -0,0 +1,42 @@
+#!/usr/local/bin/python3
+import re
+
+# write a function that checks to see if an IP address is in a provided network
+# in this example see if an IP is local
+
+my_ip = "207.224.48.249"
+
+local_networks = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
+    "207.224.48.248/24"
+]
+
+def check_in_networks(ip, networks):
+    print(re.match(r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$', ip).groups())
+    (a,b,c,d) = tuple(map(int, re.match(r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$', ip).groups()))
+    test_ip_number = (a << 24) + (b<<16) + (c<<8) + (d)
+
+    #print("one:{0:08b}.{1:08b}.{2:08b}.{3:08b}".format(a,b,c,d))
+    #print("two:{0:032b}".format(test_ip_number))
+
+    for net in networks:
+        (na,nb,nc,nd,sn) = tuple(map(int, re.match(r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\/(\d+)$', net).groups()))
+        network_ip_number = (na << 24) + (nb<<16) + (nc<<8) + (nd)
+        # start at msb and work backward until i'm out of bits i care about
+        net_mask_sum = 0;
+        ip_mask_sum  = 0;
+        for bit in range(32,32-sn,-1):
+            my_bit = ( 1<< (32-sn))
+            net_mask_sum += my_bit % network_ip_number
+            ip_mask_sum += my_bit % test_ip_number
+
+        if net_mask_sum == ip_mask_sum:
+            return True
+    return False
+
+
+t = check_in_networks(my_ip, local_networks)
+
+print("%s\nIn: %r\nFrom: %s" % (my_ip, t, str(local_networks)))
